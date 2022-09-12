d60bb1e18a08c2f01e03798df70114241f8a7c17
Nika Perfilova <ees2009.2014@gmail.com>
2022-09-12 23:06:01 +0700

    fix: improve footer layout

--- src/components/Footer/component.jsx
+++ src/components/Footer/component.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Wrapper from '@/components/Wrapper/component';
+import { ReactComponent as Icon } from '@/assets/icons/github.svg';
+import classes from './styles.module.scss';
 
 const Footer = () => (
   <footer>
     <Wrapper>
-      <a href="https://github.com/npwsk" target="_blank" rel="noopener noreferrer">
-        npwsk
-      </a>
-      2022
+      <div className={classes.content}>
+        <a href="https://github.com/npwsk" target="_blank" rel="noopener noreferrer">
+          <Icon />
+          {/* npwsk */}
+        </a>
+        2022
+      </div>
     </Wrapper>
   </footer>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [svgr(), react(),],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
