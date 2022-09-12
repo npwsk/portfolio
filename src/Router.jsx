2c3f781f3347f37bab1ff0af216907991339ff70
Nika Perfilova <ees2009.2014@gmail.com>
2022-09-12 22:49:41 +0700

    fix: hide routes which are not ready

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
-import About from '@/pages/About';
-import Projects from '@/pages/Projects';
+// import About from '@/pages/About';
+// import Projects from '@/pages/Projects';
 // import Experience from '@/pages/Experience';
-import Education from '@/pages/Education';
+// import Education from '@/pages/Education';
 import Contacts from '@/pages/Contacts';
 import NotFound from '@/pages/NotFound';
-import Project from '@/components/Project';
+// import Project from '@/components/Project';
 import Layout from '@/components/Layout';
 
 const Router = () => (
@@ -15,13 +15,13 @@ const Router = () => (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="projects" element={<Projects />}>
+        {/* <Route path="about" element={<About />} /> */}
+        {/* <Route path="projects" element={<Projects />}>
           <Route path=":projectId" element={<Project />} />
           <Route index element={<div>Select a project</div>} />
-        </Route>
+        </Route> */}
         {/* <Route path="experience" element={<Experience />} /> */}
-        <Route path="education" element={<Education />} />
+        {/* <Route path="education" element={<Education />} /> */}
         <Route path="contacts" element={<Contacts />} />
         <Route path="*" element={<NotFound />} />
       </Route>
--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,9 @@
 
 export const navItems = [
   { id: 'home_navlink', route: '/', text: 'Home', key: 'home' },
-  { id: 'about_navlink', route: '/about', text: 'About me', key: 'about' },
-  { id: 'projects_navlink', route: '/projects', text: 'Projects', key: 'projects' },
+  // { id: 'about_navlink', route: '/about', text: 'About me', key: 'about' },
+  // { id: 'projects_navlink', route: '/projects', text: 'Projects', key: 'projects' },
   // { id: 'experience_navlink', route: '/experience', text: 'Experience', key: 'experience' },
-  { id: 'education_navlink', route: '/education', text: 'Education', key: 'education' },
+  // { id: 'education_navlink', route: '/education', text: 'Education', key: 'education' },
   { id: 'contacts_navlink', route: '/contacts', text: 'Contacts', key: 'contacts' },
 ];
